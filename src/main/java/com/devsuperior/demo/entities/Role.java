4c3a6d2bab997231e07735c18d5ca85962385f28
Wellington Lima <wellington.map@hotmail.com>
2024-11-27 17:35:45 -0300

    feat: add Spring security and permit global access to any route

--- src/main/java/com/devsuperior/demo/entities/Role.java
+++ src/main/java/com/devsuperior/demo/entities/Role.java
@@ -1,15 +1,9 @@
 package com.devsuperior.demo.entities;
 
-import java.util.HashSet;
-import java.util.Set;
-
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.JoinTable;
-import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
 
 @Entity
